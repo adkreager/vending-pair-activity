b900587f3e4e8caf70159d6313a0199cd926cb46
nickorfitelli <nicholas.orfitelli.1@us.af.mil>
2020-08-06 15:25:10 -0400

    first 6 test

--- src/main/java/com/galvanize/VendingMachine.java
+++ src/main/java/com/galvanize/VendingMachine.java
@@ -1,4 +1,42 @@
 package com.galvanize;
 
+import java.util.ArrayList;
+
+
+
 public class VendingMachine {
+    private ArrayList<Item> items = new ArrayList<Item>();
+    private String[] codes = {"123","321","000"};
+    public double profit = 0.0;
+    private double depositedFunds = 0.0;
+
+    public ArrayList<com.galvanize.Item> getItems() {
+
+        return this.items;
+    }
+
+    public void addItem(Item item) {
+        this.items.add(item);
+    }
+
+    public double getProfit() {
+        return this.profit;
+    }
+
+    public void depositMoney(double money){
+        this.depositedFunds += money;
+    }
+
+    public double getDepositedFunds() {
+        return this.depositedFunds;
+    }
+
+    public String enterCode(String code) {
+        for(int i = 0; i < this.codes.length; i++){
+            if(this.codes[i] == code){
+                return "OK";
+            }
+        }
+        return "Try Again!";
+    }
 }
--- src/test/java/com/galvanize/VendingMachineTest.java
+++ src/test/java/com/galvanize/VendingMachineTest.java
@@ -1,4 +1,68 @@
 package com.galvanize;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class VendingMachineTest {
+
+    @Test
+    public void canSeeWhatIsInVendingMachine(){
+        VendingMachine vend = new VendingMachine();
+
+        ArrayList<Item> list = new ArrayList<Item>();
+
+        assertEquals(list,vend.getItems());
+    }
+
+    @Test
+    public void addItemsToVendingMachine(){
+        VendingMachine vend = new VendingMachine();
+        ArrayList<Item> list = new ArrayList<Item>();
+
+        Item item1 = new Item();
+        list.add(item1);
+        vend.addItem(item1);
+
+        assertEquals(list,vend.getItems());
+    }
+
+    @Test
+    public void seeTotalSalesFromVendingMachine(){
+        VendingMachine vend = new VendingMachine();
+        assertEquals(0.0,vend.getProfit());
+
+    }
+
+    @Test
+    public void howMuchMoneyHaveIDeposited(){
+        VendingMachine vend = new VendingMachine();
+        vend.depositMoney(2.00);
+        assertEquals(2.00,vend.getDepositedFunds());
+    }
+
+    @Test
+    public void addMoreMoney(){
+        VendingMachine vend = new VendingMachine();
+        vend.depositMoney(2.00);
+        assertEquals(2.00,vend.getDepositedFunds());
+
+        vend.depositMoney(0.25);
+        assertEquals(2.25,vend.getDepositedFunds());
+
+        vend.depositMoney(0.50);
+        assertEquals(2.75,vend.getDepositedFunds());
+    }
+
+    @Test
+    public void errorCode(){
+        VendingMachine vend = new VendingMachine();
+        assertEquals("OK",vend.enterCode("123"));
+        assertEquals("Try Again!",vend.enterCode("12345"));
+
+    }
+
+
 }
